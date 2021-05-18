cf438c5c479c099373c6c039ad3879a47e3f9847
dineshsonachalam <dineshsonachalam@gmail.com>
2021-05-19 00:45:13 +0530

    Code cleanup

--- frontend/node_modules/env-cmd/dist/parse-rc-file.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Gets the env vars from the rc file and rc environments
- */
-export declare function getRCFileVars({ environments, filePath }: {
-    environments: string[];
-    filePath: string;
-}): Promise<{
-    [key: string]: any;
-}>;
