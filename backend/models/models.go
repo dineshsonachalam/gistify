34027d12cc3267bcd68eb9c30630471016df6d9b
dineshsonachalam <dineshsonachalam@gmail.com>
2021-07-20 21:42:41 +0530

    code cleanup

--- backend/models/models.go
+++ backend/models/models.go
@@ -48,9 +48,8 @@ func GetUser(databaseUrl string, id string) (bool, string) {
 	err = conn.QueryRow(context.Background(), "select id, username from app_user where id=$1", id).Scan(&githubUserID, &username)
 	if err != nil || githubUserID != id {
 		return false, username
-	} else {
-		return true, username
 	}
+	return true, username
 }
 
 func CreateGist(databaseUrl string, id string, filename string, generatedFrom string, url string, githubUserID string) bool {
@@ -83,29 +82,27 @@ func GetAllGist(databaseUrl string, githubUserID string) []map[string]interface{
 	rows, err := conn.Query(context.Background(), "SELECT id, filename, generatedFrom, url, date_trunc('second', createdAt) as createdAt FROM gist WHERE githubUserID=$1 ORDER BY createdAt DESC NULLS LAST", githubUserID)
 	if err != nil {
 		return gistData
-	} else {
-		defer rows.Close()
-		for rows.Next() {
-			var id string
-			var filename string
-			var generatedFrom string
-			var url string
-			var createdAt time.Time
-			err = rows.Scan(&id, &filename, &generatedFrom, &url, &createdAt)
-			if err != nil {
-				return gistData
-			} else {
-				var gist = make(map[string]interface{})
-				gist["key"] = id
-				gist["filename"] = filename
-				gist["generatedFrom"] = generatedFrom
-				gist["url"] = url
-				gist["createdAt"] = createdAt
-				gistData = append(gistData, gist)
-			}
-		}
-		return gistData
 	}
+	defer rows.Close()
+	for rows.Next() {
+		var id string
+		var filename string
+		var generatedFrom string
+		var url string
+		var createdAt time.Time
+		err = rows.Scan(&id, &filename, &generatedFrom, &url, &createdAt)
+		if err != nil {
+			return gistData
+		}
+		var gist = make(map[string]interface{})
+		gist["key"] = id
+		gist["filename"] = filename
+		gist["generatedFrom"] = generatedFrom
+		gist["url"] = url
+		gist["createdAt"] = createdAt
+		gistData = append(gistData, gist)
+	}
+	return gistData
 }
 
 func GetGist(databaseUrl string, gistID string) map[string]interface{} {
@@ -118,25 +115,23 @@ func GetGist(databaseUrl string, gistID string) map[string]interface{} {
 	rows, err := conn.Query(context.Background(), "SELECT id, filename, generatedFrom, url, date_trunc('second', createdAt) as createdAt FROM gist WHERE id=$1", gistID)
 	if err != nil {
 		return gistData
-	} else {
-		defer rows.Close()
-		for rows.Next() {
-			var id string
-			var filename string
-			var generatedFrom string
-			var url string
-			var createdAt time.Time
-			err = rows.Scan(&id, &filename, &generatedFrom, &url, &createdAt)
-			if err != nil {
-				return gistData
-			} else {
-				gistData["key"] = id
-				gistData["filename"] = filename
-				gistData["generatedFrom"] = generatedFrom
-				gistData["url"] = url
-				gistData["createdAt"] = createdAt
-			}
-		}
-		return gistData
 	}
+	defer rows.Close()
+	for rows.Next() {
+		var id string
+		var filename string
+		var generatedFrom string
+		var url string
+		var createdAt time.Time
+		err = rows.Scan(&id, &filename, &generatedFrom, &url, &createdAt)
+		if err != nil {
+			return gistData
+		}
+		gistData["key"] = id
+		gistData["filename"] = filename
+		gistData["generatedFrom"] = generatedFrom
+		gistData["url"] = url
+		gistData["createdAt"] = createdAt
+	}
+	return gistData
 }
