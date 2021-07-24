27c6eeae6e0cc4c1a82cd7d7f99d70008ba5bef9
Dinesh Sonachalam <dineshsonachalam@gmail.com>
2021-07-24 19:02:58 +0530

    Update dev-startup.sh

--- dev-startup.sh
+++ dev-startup.sh
@@ -2,8 +2,9 @@
 echo "127.0.0.1 postgres" | sudo tee -a /etc/hosts
 echo "💾 Installing frontend dependencies"
 npm i --prefix frontend
-echo "🚀 Starting frondend app"
+echo "🚀 Starting frontend and backend app"
 npm run start --prefix frontend &
+cd backend && go run main.go &
 echo "📦📦📦 Running docker-compose"
 docker-compose down 
 docker-compose up 
